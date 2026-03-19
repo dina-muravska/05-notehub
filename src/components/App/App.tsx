55539a966770de4cd1fe911d9b80f142e58b1d51
Dina Muravska <dina.muravska.25@gmail.com>
2026-03-19 23:38:58 +0200

    add basic component markup

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,5 @@
 import NoteList from "../NoteList/NoteList";
+import SearchBox from "../SearchBox/SearchBox";
 import css from "./App.module.css";
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
       <div className={css.app}>
         <header className={css.toolbar}>
           <button className={css.button}>Create note +</button>
-
+          <SearchBox />
           <NoteList />
         </header>
       </div>
