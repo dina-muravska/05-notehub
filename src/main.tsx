8689d5a81319eb644e34c2552a004ab60e0ab354
Dina Muravska <dina.muravska.25@gmail.com>
2026-03-19 20:15:03 +0200

    fix files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "modern-normalize";
-import "./global.css";
 import "./index.css";
 import App from "./App.tsx";
 
