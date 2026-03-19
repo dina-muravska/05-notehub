f3edb1454b4316c6e9e1d384cbeefe2868d9cfdd
Dina Muravska <dina.muravska.25@gmail.com>
2026-03-19 23:32:30 +0200

    add components pagination and noteform

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import NoteList from "../NoteList/NoteList";
+import css from "./App.module.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div className={css.app}>
+        <header className={css.toolbar}>
+          <button className={css.button}>Create note +</button>
+
+          <NoteList />
+        </header>
+      </div>
+    </div>
+  );
 }
 
 export default App;
