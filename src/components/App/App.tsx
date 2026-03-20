140b7ad2f07c2b459113dbee846c62f873aa36e7
Dina Muravska <dina.muravska.25@gmail.com>
2026-03-21 00:32:36 +0200

    add all component in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,63 @@
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useDebouncedCallback } from "use-debounce";
+
+import { fetchNotes } from "../../services/noteService";
+
 import NoteList from "../NoteList/NoteList";
+import Pagination from "../Pagination/Pagination";
 import SearchBox from "../SearchBox/SearchBox";
+import Modal from "../Modal/Modal";
+import NoteForm from "../NoteForm/NoteForm";
+
 import css from "./App.module.css";
 
-function App() {
+export default function App() {
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    setSearch(value);
+    setPage(1);
+  }, 500);
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["notes", page, search],
+    queryFn: () => fetchNotes({ page, search }),
+  });
+
+  const notes = data?.notes ?? [];
+  const totalPages = data?.totalPages ?? 0;
+
   return (
-    <div>
-      <div className={css.app}>
-        <header className={css.toolbar}>
-          <button className={css.button}>Create note +</button>
-          <SearchBox />
-          <NoteList />
-        </header>
-      </div>
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        <SearchBox onSearch={handleSearch} />
+
+        {totalPages > 1 && (
+          <Pagination
+            totalPages={totalPages}
+            currentPage={page}
+            onPageChange={setPage}
+          />
+        )}
+
+        <button className={css.button} onClick={() => setIsOpen(true)}>
+          Create note +
+        </button>
+      </header>
+
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error loading notes</p>}
+
+      {notes.length > 0 && <NoteList notes={notes} />}
+
+      {isOpen && (
+        <Modal onClose={() => setIsOpen(false)}>
+          <NoteForm onClose={() => setIsOpen(false)} />
+        </Modal>
+      )}
     </div>
   );
 }
-
-export default App;
