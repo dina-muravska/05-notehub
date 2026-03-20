a25590585072c0dc337d9c2301483f9070292922
Dina Muravska <dina.muravska.25@gmail.com>
2026-03-20 23:45:15 +0200

    add types

---
+++ src/types/note.ts
@@ -0,0 +1,10 @@
+export type NoteTag = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+
+export interface Note {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  updateAt: string;
+  tag: NoteTag;
+}
